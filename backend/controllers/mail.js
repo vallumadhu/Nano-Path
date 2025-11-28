a5dc240d52cce5c3a2fc37eb4a5ac71094d88229
vallumadhu <vallumadhu02@gmail.com>
2025-11-29 02:31:08 +0530

    using resend instead of nodemailer

--- backend/controllers/mail.js
+++ backend/controllers/mail.js
@@ -1,38 +1,21 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.GMAIL_ADDRESS,
-    pass: process.env.GMAIL_APP_PASS,
-  },
-});
-
-transporter.verify((err, success) => {
-  if (err) {
-    console.error('SMTP verify error:', err);
-  } else {
-    console.log('SMTP server is ready to take our messages');
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async (to, sub, msg) => {
-  try {
-    const result = await transporter.sendMail({
-      from: process.env.GMAIL_ADDRESS,
-      to,
-      subject: sub,
-      html: msg,
-    });
-    console.log('Mail sent:', result.messageId);
-    return result;
-  } catch (err) {
-    console.error('Mail error:', err);
-    throw err;
-  }
+    try {
+        const result = await resend.emails.send({
+            from: 'Orb Share <onboarding@resend.dev>',
+            to,
+            subject: sub,
+            html: msg,
+        });
+        console.log('Mail sent:', result.data.id);
+        return result;
+    } catch (err) {
+        console.error('Mail error:', err);
+        throw err;
+    }
 };
 
 module.exports = sendMail;
