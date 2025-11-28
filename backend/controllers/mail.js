fd3ed22533088b875c76e01787877046305dbed6
vallumadhu <vallumadhu02@gmail.com>
2025-11-29 01:51:12 +0530

    deployment changes

--- backend/controllers/mail.js
+++ backend/controllers/mail.js
@@ -1,30 +1,26 @@
 const nodemailer = require("nodemailer");
-require('dotenv').config()
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.GMAIL_ADDRESSS,
-        pass: process.env.GMAIL_APP_PASS
-    },
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_APP_PASS,
+  },
 });
 
 const sendMail = async (to, sub, msg) => {
-    try {
-        const info = await transporter.sendMail({
-            from: process.env.GMAIL_ADDRESSS,
-            to,
-            subject: sub,
-            html: msg,
-        });
-        console.log('Mail sent:', info.messageId);
-        return info;
-    } catch (err) {
-        console.error('Error sending mail:', err);
-        throw err;
-    }
+  try {
+    await transporter.sendMail({
+      from: process.env.GMAIL_ADDRESS,
+      to,
+      subject: sub,
+      html: msg,
+    });
+    console.log("Email sent");
+  } catch (err) {
+    console.error("Email error:", err);
+  }
 };
 
 module.exports = sendMail;
