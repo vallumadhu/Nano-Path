8e8c026aa0a349c4725ab00057fed64f5940d828
vallumadhu <vallumadhu02@gmail.com>
2025-11-29 02:18:44 +0530

    deployment changes

--- backend/controllers/mail.js
+++ backend/controllers/mail.js
@@ -1,8 +1,8 @@
-const nodemailer = require("nodemailer");
-require("dotenv").config();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: 'smtp.gmail.com',
   port: 465,
   secure: true,
   auth: {
@@ -11,17 +11,27 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+transporter.verify((err, success) => {
+  if (err) {
+    console.error('SMTP verify error:', err);
+  } else {
+    console.log('SMTP server is ready to take our messages');
+  }
+});
+
 const sendMail = async (to, sub, msg) => {
   try {
     const result = await transporter.sendMail({
-      from: `"NanoPath" <${process.env.GMAIL_ADDRESS}>`,
+      from: process.env.GMAIL_ADDRESS,
       to,
       subject: sub,
       html: msg,
     });
-    console.log("Mail sent:", result);
+    console.log('Mail sent:', result.messageId);
+    return result;
   } catch (err) {
-    console.error("Mail error:", err);
+    console.error('Mail error:', err);
+    throw err;
   }
 };
 
